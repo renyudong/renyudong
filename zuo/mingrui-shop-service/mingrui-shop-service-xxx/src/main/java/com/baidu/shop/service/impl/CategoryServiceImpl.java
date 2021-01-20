65c2b8dd290ebebc825989d6ffa56116ed4f1228
renyudong <renyudong2001@163.com>
2021-01-20 19:22:49 +0800

    作业2

--- zuo/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ zuo/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -5,8 +5,11 @@ import com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.serivce.CategoryService;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
@@ -25,5 +28,62 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
         List<CategoryEntity> list = categoryMapper.select(categoryEntity);
 
         return this.setResultSuccess(list);
+    }
+
+    @Transactional //以后增删改方法必须加这个注解
+    @Override
+    public Result<JsonObject> deleteCategoryById(Integer id) {
+
+        //校验id是否合法
+        if(id == null || id <= 0) return this.setResultError("id不合法");
+
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+
+        if(categoryEntity == null) return this.setResultError("数据不存在");
+
+        //判断当前节点是否为父节点(安全!)
+        if(categoryEntity.getIsParent() == 1) return this.setResultError("当前节点为父错节点");//return之后的代码不会执行
+
+        //通过当前节点的父节点id 查询 当前节点(将要被删除的节点)的父节点下是否还有其他子节点
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId", categoryEntity.getParentId());
+        List<CategoryEntity> categoryList = categoryMapper.selectByExample(example);
+
+        //如果size <= 1 --> 如果当前节点被删除的话 当前节点的父节点下没有节点了 --> 将当前节点的父节点状态改为叶子节点
+        if(categoryList.size() <= 1){
+
+            CategoryEntity updateCategoryEntity = new CategoryEntity();
+            updateCategoryEntity.setIsParent(0);
+            updateCategoryEntity.setId(categoryEntity.getParentId());
+
+            categoryMapper.updateByPrimaryKeySelective(updateCategoryEntity);
+        }
+        //通过id删除节点
+        categoryMapper.deleteByPrimaryKey(id);
+
+        return this.setResultSuccess();
+    }
+
+    //修改
+    @Transactional
+    @Override
+    public Result<JsonObject> updateCategoryById(CategoryEntity categoryEntity) {
+
+        categoryMapper.updateByPrimaryKeySelective(categoryEntity);
+        return this.setResultSuccess();
+    }
+
+    //品牌新增
+    @Transactional
+    @Override
+    public Result<JsonObject> addCategoryById(CategoryEntity categoryEntity) {
+
+        CategoryEntity parentCategoryEntity = new CategoryEntity();
+        parentCategoryEntity.setId(categoryEntity.getParentId());
+        parentCategoryEntity.setIsParent(1);
+        categoryMapper.updateByPrimaryKeySelective(parentCategoryEntity);
+
+        categoryMapper.insertSelective(categoryEntity);
+        return this.setResultSuccess();
     }
 }
