c7c482dfc754f7e75332b6d82b406d7da37c468d
renyudong <renyudong2001@163.com>
2021-01-27 19:53:02 +0800

    作业7

--- zuo/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/serivce/SpecificationService.java
+++ zuo/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/serivce/SpecificationService.java
@@ -1,11 +1,14 @@
 package com.baidu.shop.serivce;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.dto.SpecGroupDTO;
 import com.baidu.shop.entity.SpecGroupEntity;
+import com.baidu.shop.validate.group.MingruiOperation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,4 +18,16 @@ public interface SpecificationService {
     @ApiOperation(value = "通过条件查询规格组")
     @GetMapping(value = "specgroup/list")
     Result<List<SpecGroupEntity>> getSpecGroup(SpecGroupDTO specGroupDTO);
+
+    @PostMapping(value = "specgroup/add")
+    @ApiOperation(value = "新增")
+    Result<JSONObject> addSpecGroup(@Validated(MingruiOperation.Add.class) @RequestBody SpecGroupDTO specGroupDTO);
+
+    @PutMapping(value = "specgroup/add")
+    @ApiOperation(value = "修改")
+    Result<JSONObject> updateSpecGroup(@Validated(MingruiOperation.Update.class) @RequestBody SpecGroupDTO specGroupDTO);
+
+    @DeleteMapping(value = "specgroup/delete")
+    @ApiOperation(value = "删除")
+    Result<JSONObject> deleteSpecGroup(Integer id);
 }
--- zuo/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
+++ zuo/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.baidu.shop.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.dto.SpecGroupDTO;
@@ -8,6 +9,7 @@ import com.baidu.shop.mapper.SpecGroupMapper;
 import com.baidu.shop.serivce.SpecificationService;
 import com.baidu.shop.utils.BaiduBeanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.entity.Example;
 
@@ -27,5 +29,30 @@ public class SpecificationServiceImpl extends BaseApiService implements Specific
 
         List<SpecGroupEntity> specGroupEntities = specGroupMapper.selectByExample(example);
         return this.setResultSuccess(specGroupEntities);
+    }
+
+    @Override
+    @Transactional
+    public Result<JSONObject> addSpecGroup(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.insertSelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+
+        return this.setResultSuccess();
+    }
+
+    @Override
+    @Transactional
+    public Result<JSONObject> updateSpecGroup(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.updateByPrimaryKeySelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+
+        return this.setResultSuccess();
+    }
+
+
+    @Override
+    @Transactional
+    public Result<JSONObject> deleteSpecGroup(Integer id) {
+
+        specGroupMapper.deleteByPrimaryKey(id);
+        return this.setResultSuccess();
     }
 }
