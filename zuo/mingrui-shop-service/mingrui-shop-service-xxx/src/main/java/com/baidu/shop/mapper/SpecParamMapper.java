00e4af6552a8b07cf5a39d479a780cd45cf4fb1a
renyudong <renyudong2001@163.com>
2021-03-12 08:26:17 +0800

    文档

--- zuo/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/mapper/SpecParamMapper.java
+++
@@ -1,7 +0,0 @@
-package com.baidu.shop.mapper;
-
-import com.baidu.shop.entity.SpecParamEntity;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface  SpecParamMapper extends Mapper<SpecParamEntity> {
-}
